e349b540ea4d975a1ea4a93ee0e2460e4437685d
Jack Wiggall <72393388+jackwiggall@users.noreply.github.com>
2025-08-23 15:35:40 +0100

    removes answer from guess list

--- Scripts/removeAnswers.py
+++ Scripts/removeAnswers.py
@@ -1,8 +1,10 @@
 import csv
+
 newAnswers = ".//Dictionaries/NewAnswers.csv"
 Guesses = ".//Dictionaries/Guesses.csv"
 
 try:
+	guessList = [] #list of guessable words
 	with open(newAnswers, 'r') as nA, open(Guesses, 'r') as g:
 
 		wordReader = csv.reader(nA) #NewAnswers object
@@ -10,14 +12,22 @@ try:
 		
 		guessReader = csv.reader(g) #Guess object
 		guesses = next(guessReader) #Read word list from guesses
-		
-		x=-1		
-
+			
 		for guess in guesses:
-			x=x+1
+			found=""
 			for word in words:
-				if word==guess:
-					print(str(x)+" "+word)
+				if guess==word:
+					found=word
+			if found=="":
+				guessList.append(guess) #add to list of words
+				found=""
 
+	with open(Guesses, 'w') as nG:
+		printList=""		
+		for word in guessList:
+			if word!="":
+				printList += word+","
+		nG.write(printList)
+					
 except IOError as e:
     print('Operation failed: %s' % e.strerror)
