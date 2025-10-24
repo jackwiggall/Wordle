a4ca5fb22d9e562ce816eb8cd4dafaf355d34799
Jack Wiggall <72393388+jackwiggall@users.noreply.github.com>
2025-10-24 14:45:45 +0100

    buttons to select character useage
    converts the list of characters to buttons which can be pressed to toggle if a word has been used or not

--- Site/src/Display.js
+++ Site/src/Display.js
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 
-const alphabet = ['A','B','C','D','E','F','G','H','I','J','','K','L','M','N','O','P','Q','R','S','','T','U','V','W','X','Y','Z'];
-const keyboard = ['Q','W','E','R','T','Y','U','I','O','P','','A','S','D','F','G','H','J','K','L','','Z','X','C','V','B','N','M'];
-
 function Display() {
+
+  const alphabet = ['A','B','C','D','E','F','G','H','I','J','','K','L','M','N','O','P','Q','R','S','','T','U','V','W','X','Y','Z'];
+  const keyboard = ['Q','W','E','R','T','Y','U','I','O','P','','A','S','D','F','G','H','J','K','L','','Z','X','C','V','B','N','M'];
+
   const [order, setOrder] = useState("kb");
+  const [kbUsed, setKBUsed] = useState([false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]);
+  const [abUsed, setABUsed] = useState([false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]);
+
   const keyboardList = keyboard.map((a,id) => {
       if(a!=='') {
-        return <li key={id}>{a}</li>
+        if (kbUsed[id]===false) {
+          return <li key={id}><button style={{fontSize: "40px"}} onClick={() => {setUsed(id)}}>{a}</button></li>
+        }else {
+          return <li key={id}><button style={{fontSize: "40px",background:"gray"}} onClick={() => {setUsed(id)}}>{a}</button></li>
+        }
       }else {
         return <br/>
       }
@@ -15,24 +23,55 @@ function Display() {
   );
   const alphabetList = alphabet.map((a,id) => {
       if(a!=='') {
-        return <li key={id}>{a}</li>
+        if (abUsed[id]===false) {
+          return <li key={id}><button style={{fontSize: "40px"}} onClick={() => {setUsed(id)}}>{a}</button></li>
+        }else {
+          return <li key={id}><button style={{fontSize: "40px",background:"gray"}} onClick={() => {setUsed(id)}}>{a}</button></li>
+        }
       }else {
         return <br/>
       }
     }
   );
+
+  function setUsed(id) {
+    if (order==="kb") {
+      let newList = kbUsed;
+      newList[id] = !newList[id];
+      setKBUsed(newList);
+      for (let i = 0; i < alphabet.length; ++i) {
+        if (alphabet[i]===keyboard[id]) {
+          let otherList = abUsed;
+          otherList[i] = !otherList[i];
+          setABUsed(otherList);
+        }
+      }
+    }else {
+      let newList = abUsed;
+      newList[id] = !newList[id];
+      setABUsed(newList);
+      for (let i = 0; i < keyboard.length; ++i) {
+        if (keyboard[i]===alphabet[id]) {
+          let otherList = kbUsed;
+          otherList[i] = !otherList[i];
+          setKBUsed(otherList);
+        }
+      }
+    }
+  }
+
   if (order==="kb") {
     return (
       <div className="Display">
         <ul>{keyboardList}</ul>
-        <button onClick={() => {setOrder("ab")}}>Change Order</button>
+        <button onClick={() => {setOrder("ab");}}>Change Order</button>
       </div>
     )
   }else {
     return (
       <div className="Display">
         <ul>{alphabetList}</ul>
-        <button onClick={() => {setOrder("kb")}}>Change Order</button>
+        <button onClick={() => {setOrder("kb");}}>Change Order</button>
       </div>
     )
   }
